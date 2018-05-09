081f365a9e0e43fa3f8c6c0fc090666d1dff7826
Devin Haslam <dhasl002@odu.edu>
2018-05-09 10:40:42 -0400

    implemented data shuffling

--- tf_model.py
+++ tf_model.py
@@ -1,211 +1,220 @@
 import tensorflow as tf
 import numpy as np
+from tensorflow.python import debug as tf_debug
+from tf_model import model
+from tensorflow.python import debug as tf_debug
+from random import *
+sess = tf.InteractiveSession()
 
-def conv3d_dilation(tempX, tempFilter):
-  return tf.layers.conv3d(tempX, filters = tempFilter, kernel_size=[3, 3, 1], strides=1, padding='SAME', dilation_rate = 2)
 
-def conv3d(tempX, tempW):
-  return tf.nn.conv3d(tempX, tempW, strides=[1, 2, 2, 2, 1], padding='SAME')
+def printEpoch(curEpoch):
+  print('epoch Number: ' + str(curEpoch))
+  fStat.write('epoch Number: ' + str(curEpoch) + '\n')
 
-def conv3d_s1(tempX, tempW):
-  return tf.nn.conv3d(tempX, tempW, strides=[1, 1, 1, 1, 1], padding='SAME')
+def printBatchStats(train_accuracy, CurCross):
+  fStat.write('training accuracy %g' % (train_accuracy))
+  fStat.write('\n')
+  fStat.write('Loss %g' % (CurCross))
+  fStat.write('\n')
+  print('training accuracy %g' % (train_accuracy))
+  print('Loss %g' % (CurCross))
 
-def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
+def train(trainingListLocation, batch_Size):
+  proteinList = []
+  numTraining = []
+  #open list that contains information about training (protein name and number of files)
+  with open(trainingListLocation) as trainingList:
+    next(trainingList)
+	#extract protein name and number of files
+    for newLine in trainingList:
+      proteinName, numFiles = newLine.strip().split(",")
+      proteinList.append(proteinName)
+      numTraining.append(numFiles)
+  trainProtein(numFiles, batch_Size, proteinList, numTraining)
 
-def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
+def trainProtein(numFiles, batch_Size, proteinList, numTraining):
+  for currIt in range(0, (len(proteinList)*int(numTraining[0]))):
+    it = 0
+    if (int(currIt)%batch_Size == 0):
+      a = np.zeros((batch_Size,N,P,M,1),dtype = float)
+      b = np.zeros((batch_Size,N*M*P,3), dtype = float)
+      c = np.zeros((batch_Size,N,P,M,3), dtype = float)
+    randNum = randint(0, (len(proteinList)-1))
+    randNum2 = randint(0, int(numTraining[randNum]))
+    path = "/home/dhaslam/New_test_samples2/RotatedSet/" + proteinList[randNum] + "/" + proteinList[randNum] + "-"
+    with open(path + str(randNum2) + ".txt") as inf:
+      for line in inf:
+        xCoord, yCoord, zCoord, thresh, label = line.strip().split(",")
+        xCoord = int(xCoord)
+        yCoord = int(yCoord)
+        zCoord = int(zCoord)
+        thresh = float(thresh)
+        label = int(label)	
+        a[int(currIt)%batch_Size][int(it/(32*7))][int(it/32)%7][int(it%32)][0] = thresh
+        b[int(currIt)%batch_Size][it][label] = 1
+        it = it + 1		  
+    if(int(currIt)%batch_Size == (batch_Size)-1):
+      runTrainingBatch(a, b, c)
 
-def max_pool_3x3(x):
-  return tf.nn.max_pool3d(x, ksize=[1, 3, 3, 3, 1],strides=[1, 2, 2, 2, 1], padding='SAME')
+def runTrainingBatch(a, b, c):
+  keep_prob = tf.placeholder(tf.float32)
+  CurCross = cross_entropy.eval(feed_dict={x_image: a, y_: b, x_image_2: c})
+  train_accuracy = accuracy.eval(feed_dict={x_image: a, y_: b, x_image_2: c, keep_prob: .5})
+  train_step.run(feed_dict={x_image: a, y_: b, x_image_2: c, keep_prob: .5})
+  temp = sess.run(modelResult, feed_dict={x_image: a, x_image_2: c})
+  f5 = open("/home/dhaslam/New_test_samples2/RotatedSet/labels/results3.txt","w+")  
+  for i in range(0, 7167):
+    if(temp[i][0] > temp[i][1] and temp[i][0] > temp[i][2]):
+      f5.write(str(int(i/(32*7))) + " " + str(int(i/7)%32) + " " + str(int(i%7)) + " 0\r\n")
+    if(temp[i][1] > temp[i][0] and temp[i][1] > temp[i][2]):
+      f5.write(str(int(i/(32*7))) + " " + str(int(i/7)%32) + " " + str(int(i%7)) + " 1\r\n")
+    if(temp[i][2] > temp[i][0] and temp[i][2] > temp[i][1]):
+      f5.write(str(int(i/(32*7))) + " " + str(int(i/7)%32) + " " + str(int(i%7)) + " 2\r\n")
+  f5.close()
+  printBatchStats(train_accuracy, CurCross)
 
-def model(x_image):
-  #STEM
-  with tf.name_scope("1stKernel"):
-    W_conv1 = weight_variable([3, 3, 1, 3, 32])
-  with tf.name_scope("1stConv"):
-    h_conv1 = conv3d(x_image, W_conv1)
-  #second convolution
-  W_conv2 = weight_variable([3, 3, 4, 32, 64])
-  with tf.name_scope("2ndConv"):
-    h_conv2 = conv3d_s1(h_conv1, W_conv2)
-  #third convolution path 1
-  W_conv3_A = weight_variable([1, 1, 1, 64, 64])
-  h_conv3_A = conv3d_s1(h_conv2, W_conv3_A)
-  #third convolution path 2
-  W_conv3_B = weight_variable([1, 1, 1, 64, 64])
-  h_conv3_B = conv3d_s1(h_conv2, W_conv3_B)
-  #fourth convolution path 1
-  W_conv4_A = weight_variable([3, 3, 1, 64, 96])
-  h_conv4_A = conv3d_s1(h_conv3_A, W_conv4_A)
-  #fourth convolution path 2
-  W_conv4_B = weight_variable([1, 7, 1, 64, 64])
-  h_conv4_B = conv3d_s1(h_conv3_B, W_conv4_B)
-  #fifth convolution path 2
-  W_conv5_B = weight_variable([7, 1, 1, 64, 64])
-  h_conv5_B = conv3d_s1(h_conv4_B, W_conv5_B)
-  #sixth convolution path 2
-  W_conv6_B = weight_variable([3, 3, 1, 64, 96])
-  h_conv6_B = conv3d_s1(h_conv5_B, W_conv6_B)
-  #concatenation
-  layer1 = tf.concat([h_conv4_A, h_conv6_B],4)
-  w = tf.Variable(tf.constant(1.,shape=[2,2,4,3,192]))
-  DeConnv1 = tf.nn.conv3d_transpose(layer1, filter = w, output_shape = tf.shape(x_image), strides = [1,2,2,2,1], padding = 'SAME')
+def getMRCDimensions(fileToOpen):
+  xMin = 100000000
+  xMax = -1
+  yMin = 100000000
+  yMax = -1
+  zMin = 100000000
+  zMax = -1
+  with open(fileToOpen) as inf3:
+    for line4 in inf3:
+      xCoord, yCoord, zCoord, thresh, label = line4.strip().split(",")
+      xCoord = int(xCoord)
+      yCoord = int(yCoord)
+      zCoord = int(zCoord)
+      if xCoord < xMin:
+        xMin = xCoord
+      if yCoord < yMin:
+        yMin = yCoord
+      if zCoord < zMin:
+        zMin = zCoord
+      if xCoord > xMax:
+        xMax = xCoord
+      if yCoord > yMax:
+        yMax = yCoord
+      if zCoord > zMax:
+        zMax = zCoord
+  return xMax, xMin, yMax, yMin, zMax, zMin
+
+def test():
+  keep_prob = tf.placeholder(tf.float32)
+  xMax, xMin, yMax, yMin, zMax, zMin = getMRCDimensions(path2)
+  xLength = xMax-xMin+1
+  yLength = yMax-yMin+1
+  zLength = zMax-zMin+1
+  numLines = (xLength)*(yLength)*(zLength)
+  with open(path2) as inf2:
+    a = np.zeros((1,xLength, yLength, zLength, 1),dtype = float)
+    b = np.zeros((1,numLines, 3), dtype = float)
+    c = np.zeros((1,xLength, yLength, zLength, 3), dtype = float)
+    axisZ = np.zeros((1,numLines),dtype = float)
+    axisY = np.zeros((1,numLines),dtype = float)
+    axisX = np.zeros((1,numLines),dtype = float)
+    for x in range(0, xLength):
+      for y in range(0, yLength):
+        for z in range(0, zLength):
+          curPos = (x)*yLength*zLength+(y)*zLength+(z)
+          axisX[0][curPos] = x+xMin
+          axisY[0][curPos] = y+yMin
+          axisZ[0][curPos] = z+zMin
+    next(inf2)
+    for line3 in inf2:
+      xCoord, yCoord, zCoord, thresh, label = line3.strip().split(",")
+      xCoord = int(xCoord)
+      yCoord = int(yCoord)
+      zCoord = int(zCoord) 
+      thresh = float(thresh)
+      label = int(label)
+      curPos = (xCoord-xMin)*yLength*zLength+(yCoord-yMin)*zLength+(zCoord-zMin)
+      a[0][xCoord-xMin][yCoord-yMin][zCoord-zMin][0]  = thresh
+      b[0][curPos][label] = 1
+  print(sess.run(accuracy, feed_dict={x_image: a, y_: b, x_image_2: c, keep_prob: 1.0}))
+  predictedLabels = sess.run(tf.argmax(modelResult,1), feed_dict={x_image: a, x_image_2: c})
+  writePredictionsToFile(predictedLabels, numLines, axisX, axisY, axisZ, b)
   
-  r = tf.nn.relu(layer1)
+def testSmallImages():
+  for i in range(0, 30):
+    path = path3 + str(i) + ".txt"
+    keep_prob = tf.placeholder(tf.float32)
+    xMax, xMin, yMax, yMin, zMax, zMin = getMRCDimensions(path)
+    xLength = xMax-xMin+1
+    yLength = yMax-yMin+1
+    zLength = zMax-zMin+1
+    numLines = (xLength)*(yLength)*(zLength)
+    with open(path) as inf2:
+      a = np.zeros((1,xLength, yLength, zLength, 1),dtype = float)
+      b = np.zeros((1,numLines, 3), dtype = float)
+      c = np.zeros((1,xLength, yLength, zLength, 3), dtype = float)
+      axisZ = np.zeros((1,numLines),dtype = float)
+      axisY = np.zeros((1,numLines),dtype = float)
+      axisX = np.zeros((1,numLines),dtype = float)
+      for x in range(0, xLength):
+        for y in range(0, yLength):
+          for z in range(0, zLength):
+            curPos = (x)*yLength*zLength+(y)*zLength+(z)
+            axisX[0][curPos] = x+xMin
+            axisY[0][curPos] = y+yMin
+            axisZ[0][curPos] = z+zMin
+      next(inf2)
+      for line3 in inf2:
+        xCoord, yCoord, zCoord, thresh, label = line3.strip().split(",")
+        xCoord = int(xCoord)
+        yCoord = int(yCoord)
+        zCoord = int(zCoord) 
+        thresh = float(thresh)
+        label = int(label)
+        curPos = (xCoord-xMin)*yLength*zLength+(yCoord-yMin)*zLength+(zCoord-zMin)
+        a[0][xCoord-xMin][yCoord-yMin][zCoord-zMin][0]  = thresh
+        b[0][curPos][label] = 1
+    acc = sess.run(accuracy, feed_dict={x_image: a, y_: b, x_image_2: c, keep_prob: 1.0})
+    print(acc)
+    fStat.write(str(acc) + '\n')
+    predictedLabels = sess.run(tf.argmax(modelResult,1), feed_dict={x_image: a, x_image_2: c})
+    writePredictionsToFile(predictedLabels, numLines, axisX, axisY, axisZ, b, i)
 
-  #INCEPTION_A
-  #first convolution path 1
-  W_conv1_A = weight_variable([3, 3, 1, 192, 192])
-  h_conv1_A = conv3d(r, W_conv1_A)
-  #first convolution path 2- pooling 3x3
-  h_conv1_B = max_pool_3x3(r)
-  #concat
-  concat1 = tf.concat([h_conv1_A, h_conv1_B],4)
-  #second convolution path 1
-  W_conv2_A = weight_variable([1, 1, 1, 384, 32])
-  h_conv2_A = conv3d_s1(concat1, W_conv2_A)
-  #second convolution path 2
-  W_conv2_B = weight_variable([1, 1, 1, 384, 32])
-  h_conv2_B = conv3d_s1(concat1, W_conv2_B)
-  #second convolution path 3
-  W_conv2_C = weight_variable([1, 1, 1, 384, 32])
-  h_conv2_C = conv3d_s1(concat1, W_conv2_C)
-  #third convolution path 2
-  W_conv3_B = weight_variable([3, 3, 1, 32, 32])
-  h_conv3_B = conv3d_s1(h_conv2_B, W_conv3_B)
-  #third convolution path 3
-  W_conv3_C = weight_variable([3, 3, 1, 32, 48])
-  h_conv3_C = conv3d_s1(h_conv2_C, W_conv3_C)
-  #fourth convolution path 3
-  W_conv4_C = weight_variable([3, 3, 1, 48, 64])
-  h_conv4_C = conv3d_s1(h_conv3_C, W_conv4_C)
-  #concat
-  concat2 = tf.concat([h_conv4_C, h_conv3_B, h_conv2_A],4)
-  #convolution after paths merge
-  W_conv5 = weight_variable([3, 3, 1, 128, 384])
-  h_conv5 = conv3d_s1(concat2, W_conv5)
-  #last convolution in inceptionA, this is a dialated convolution
-  h_conv6 = conv3d_dilation(h_conv5, 384)
-  #residual learning added to last convolution
-  #layer2 = h_conv6
-  layer2 = tf.add(h_conv6, concat1)
-  w = tf.Variable(tf.constant(1.,shape=[4,4,6,3,384]))
-  DeConnv2 = tf.nn.conv3d_transpose(layer2, filter = w, output_shape = tf.shape(x_image), strides = [1,4,4,4,1], padding = 'SAME')
-
-
-  r2 = tf.nn.relu(layer2)
-  #REDUCTION A
-  #first pool path 1
-  h_conv1_A = max_pool_3x3(r2)
-  #first convolution path 2
-  W_conv1_B = weight_variable([3, 3, 1, 384, 32])
-  h_conv1_B = conv3d(r2, W_conv1_B)
-  #first convolution path 3
-  W_conv1_C = weight_variable([1, 1, 1, 384, 256])
-  h_conv1_C = conv3d_s1 (r2, W_conv1_C)
-  #second convolution path 3
-  W_conv2_C = weight_variable([3, 3, 1, 256, 256])
-  h_conv2_C = conv3d_s1(h_conv1_C, W_conv2_C)
-  #third convolution path 3
-  W_conv3_C = weight_variable([3, 3, 1, 256, 384])
-  h_conv3_C = conv3d(h_conv2_C, W_conv3_C)
-  #last step of reduction a, concat
-  layer3 = tf.concat([h_conv1_B, h_conv1_A, h_conv3_C],4)
-
-  r3 = tf.nn.relu(layer3)
-
-  #INCEPTION_B
-  #first convolution path 1
-  W_conv1_A = weight_variable([1, 1, 1, 800, 128])
-  h_conv1_A = conv3d_s1(r3, W_conv1_A)
-  #first convolution path 2
-  W_conv1_B = weight_variable([1, 1, 1, 800, 128])
-  h_conv1_B = conv3d_s1(r3, W_conv1_B)
-  #second convolution path 2
-  W_conv2_B = weight_variable([1, 7, 1, 128, 128])
-  h_conv2_B = conv3d_s1(h_conv1_B, W_conv2_B)
-  #third convolution path 2
-  W_conv3_B = weight_variable([7, 1, 1, 128, 128])
-  h_conv3_B = conv3d_s1(h_conv2_B, W_conv3_B)
-  #second convolution path 1
-  W_conv2_A = weight_variable([1, 1, 1, 128, 896])
-  h_conv2_A = conv3d_s1(h_conv1_A, W_conv2_A)
-  #concatenation
-  concat1 = tf.concat([h_conv3_B, h_conv2_A], 4)
-  #dilation layer1
-  h_conv4 = conv3d_dilation(concat1, 800)
-  #residual addition
-  #layer4 = h_conv4
-  layer4 = tf.add(h_conv4, r3)
-  w = tf.Variable(tf.constant(1.,shape=[8,8,7,3,800]))
-  DeConnv3 = tf.nn.conv3d_transpose(layer4, filter = w, output_shape = tf.shape(x_image), strides = [1,8,8,8,1], padding = 'SAME')
-
-  r4 = tf.nn.relu(layer4)
+def writePredictionsToFile(predictedLabels, numLines, axisX, axisY, axisZ, b, index):
+  path = tempPath + str(index) + ".txt"
+  f1 = open(path,"w+")
+  for i in range(0, numLines):
+    f1.write(str(int(axisX[0][i])) + " " + str(int(axisY[0][i])) + " " + str(int(axisZ[0][i])) + " " + str(predictedLabels[i]) + "\r\n")
 
 
-  #Reduction B
-  #first convolution path 1
-  #W_conv1_A = weight_variable([1, 1, 1, 800, 192])
-  #h_conv1_A = conv3d_s1(r4, W_conv1_A)
-  #first maxpool path 2
-  h_conv1_B = max_pool_3x3(r4)
-  #first convolution path 3
-  W_conv1_C = weight_variable([1, 1, 1, 800, 256])
-  h_conv1_C = conv3d_s1(r4, W_conv1_C)
-  #second convolution path 1
-  W_conv2_A = weight_variable([3, 3, 1, 800, 32])
-  h_conv2_A = conv3d(r4, W_conv2_A)
-  #second convolution path 3
-  W_conv2_C = weight_variable([3, 3, 1, 256, 256])
-  h_conv2_C = conv3d_s1(h_conv1_C, W_conv2_C)
-  #third convolution path 3
-  #W_conv3_C = weight_variable([7, 1, 1, 256, 320])
-  #h_conv3_C = conv3d_s1(h_conv2_C, W_conv3_C)
-  #fourth convolution path 3
-  W_conv4_C = weight_variable([3, 3, 1, 256, 384])
-  h_conv4_C = conv3d(h_conv2_C, W_conv4_C)
-  #concat
-  layer5 = tf.concat([h_conv4_C, h_conv1_B, h_conv2_A], 4)
+N = 32 #x dimension of training patch size
+M = 32 #y dimension of training patch size
+P = 7 #z dimension of training patch size
+batch_Size = 30
+epochs = 10000000
+outputStats = "/home/dhaslam/New_test_samples2/RotatedSet/labels/Statistics.txt"
+trainingListLocation = "/home/dhaslam/New_test_samples2/RotatedSet/list.txt"
+path2 = "/home/dhaslam/New_test_samples2/RotatedSet/4XDA/4XDA_label.txt"
+path3 = "/home/dhaslam/New_test_samples2/RotatedSet/4XDA/4XDA-"
+tempPath = "/home/dhaslam/New_test_samples2/RotatedSet/labels/results"
+f5 = open("/home/dhaslam/New_test_samples2/RotatedSet/labels/results3.txt","w+")  
 
-  r5 = tf.nn.relu(layer5)
+x_image = tf.placeholder(tf.float32, shape=[None, None, None, None, 1])
+x_image_2 = tf.placeholder(tf.float32, shape=[None, None, None, None, 3])
+y_ = tf.placeholder(tf.float32, shape=[None, None, 3])
 
-  #INCEPTION_C
-  #first convolution path 1
-  W_conv1_A = weight_variable([1, 1, 1, 1216, 128])
-  h_conv1_A = conv3d_s1(r5, W_conv1_A)
-  #first convolution path 2
-  W_conv1_B = weight_variable([1, 1, 1, 1216, 128])
-  h_conv1_B = conv3d_s1(r5, W_conv1_B)
-  #second convolution path 1
-  W_conv2_A = weight_variable([1, 1, 1, 128, 896])
-  h_conv2_A = conv3d_s1(h_conv1_A, W_conv2_A)
-  #second convolution path 2
-  W_conv2_B = weight_variable([1, 7, 1, 128, 128])
-  h_conv2_B = conv3d_s1(h_conv1_B, W_conv2_B)
-  #third convolution path 2
-  W_conv3_B = weight_variable([7, 1, 1, 128, 128])
-  h_conv3_B = conv3d_s1(h_conv2_B, W_conv3_B)
-  #concat
-  concat1 = tf.concat([h_conv3_B,h_conv2_A],4)
-  #dilation
-  h_conv4 = conv3d_dilation(concat1, 1216)
-  #layer6 = h_conv4
-  layer6 = tf.add(h_conv4, r5)
-  w = tf.Variable(tf.constant(1.,shape=[16,16,7,3,1216]))
-  DeConnv4 = tf.nn.conv3d_transpose(layer6, filter = w, output_shape = tf.shape(x_image), strides = [1,16,16,16,1], padding = 'SAME')
+modelResult = model(x_image, x_image_2) #sets modelResults to the final result of the model 
 
-  add1 = tf.add(DeConnv1,DeConnv2)
-  add2 = tf.add(DeConnv3,DeConnv4)
-  with tf.name_scope("BeforeReshape"):
-	final = tf.add(add1, add2)
-  #with tf.name_scope("AfterMoreChannelsWV"):
-    #WV = weight_variable([1, 1, 1, 1, 3])
-  #with tf.name_scope("AfterMoreChannels"):
-    #final_conv = conv3d_s1(final, WV)
-  with tf.name_scope("AfterReshape"):
-    final_conv = tf.reshape(final, [-1, 3])
+#train model
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=modelResult, labels=y_))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(modelResult, 1), tf.argmax(tf.reshape(y_, [-1, 3]), 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-  return final_conv
+#run
+with tf.Session() as sess:
+   fStat = open(outputStats,"w+")
+   sess.run(tf.global_variables_initializer())
+   #sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+   for curEpoch in range(1, epochs):
+     printEpoch(curEpoch) 
+     train(trainingListLocation, batch_Size)
+     #test()
+     testSmallImages()
+   fStat.close()
