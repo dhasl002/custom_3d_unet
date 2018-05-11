320c3a08b706eb126418b7ce78f0be4b94d6e471
Devin Haslam <dhasl002@odu.edu>
2018-05-11 10:25:25 -0400

    Update DeepCryo.py

--- DeepCryo.py
+++ DeepCryo.py
@@ -3,6 +3,7 @@ import numpy as np
 from tensorflow.python import debug as tf_debug
 from tf_model import model
 from tensorflow.python import debug as tf_debug
+from random import *
 sess = tf.InteractiveSession()
 
 
@@ -19,22 +20,29 @@ def printBatchStats(train_accuracy, CurCross):
   print('Loss %g' % (CurCross))
 
 def train(trainingListLocation, batch_Size):
+  proteinList = []
+  numTraining = []
   #open list that contains information about training (protein name and number of files)
   with open(trainingListLocation) as trainingList:
     next(trainingList)
 	#extract protein name and number of files
     for newLine in trainingList:
       proteinName, numFiles = newLine.strip().split(",")
-      path = "/home/dhaslam/New_test_samples2/RotatedSet/" + proteinName + "/" + proteinName + "-"
-      trainProtein(path, numFiles, batch_Size)
+      proteinList.append(proteinName)
+      numTraining.append(numFiles)
+  trainProtein(numFiles, batch_Size, proteinList, numTraining)
 
-def trainProtein(path, numFiles, batch_Size):
-  for currFileNum in range(0, int(numFiles)):
+def trainProtein(numFiles, batch_Size, proteinList, numTraining):
+  for currIt in range(0, (len(proteinList)*int(numTraining[0]))):
     it = 0
-    if (int(currFileNum)%batch_Size == 0):
-      a = np.zeros((batch_Size,N,P,M,3),dtype = float)
+    if (int(currIt)%batch_Size == 0):
+      a = np.zeros((batch_Size,N,M,P,1),dtype = float)
       b = np.zeros((batch_Size,N*M*P,3), dtype = float)
-    with open(path + str(currFileNum) + ".txt") as inf:
+      c = np.zeros((batch_Size,N,M,P,3), dtype = float)
+    randNum = randint(0, (len(proteinList)-1))
+    randNum2 = randint(0, int(numTraining[randNum]))
+    path = "/home/dhaslam/New_test_samples2/RotatedSet/" + proteinList[randNum] + "/" + proteinList[randNum] + "-"
+    with open(path + str(randNum2) + ".txt") as inf:
       for line in inf:
         xCoord, yCoord, zCoord, thresh, label = line.strip().split(",")
         xCoord = int(xCoord)
@@ -42,20 +50,18 @@ def trainProtein(path, numFiles, batch_Size):
         zCoord = int(zCoord)
         thresh = float(thresh)
         label = int(label)	
-        a[int(currFileNum)%batch_Size][int(it/(32*7))][int(it/32)%7][int(it%32)][0] = thresh
-        a[int(currFileNum)%batch_Size][int(it/(32*7))][int(it/32)%7][int(it%32)][1] = thresh	
-        a[int(currFileNum)%batch_Size][int(it/(32*7))][int(it/32)%7][int(it%32)][2] = thresh  
-        b[int(currFileNum)%batch_Size][it][label] = 1
+        a[int(currIt)%batch_Size][int(it/(32*7))][int(it/32)%32][int(it%7)][0] = thresh
+        b[int(currIt)%batch_Size][it][label] = 1
         it = it + 1		  
-    if(int(currFileNum)%batch_Size == (batch_Size)-1):
-      runTrainingBatch(a, b)
+    if(int(currIt)%batch_Size == (batch_Size)-1):
+      runTrainingBatch(a, b, c)
 
-def runTrainingBatch(a, b):
+def runTrainingBatch(a, b, c):
   keep_prob = tf.placeholder(tf.float32)
-  CurCross = cross_entropy.eval(feed_dict={x_image: a, y_: b})
-  train_accuracy = accuracy.eval(feed_dict={x_image: a, y_: b, keep_prob: .5})
-  train_step.run(feed_dict={x_image: a, y_: b, keep_prob: .5})
-  temp = sess.run(modelResult, feed_dict={x_image: a})
+  CurCross = cross_entropy.eval(feed_dict={x_image: a, y_: b, x_image_2: c})
+  train_accuracy = accuracy.eval(feed_dict={x_image: a, y_: b, x_image_2: c, keep_prob: .5})
+  train_step.run(feed_dict={x_image: a, y_: b, x_image_2: c, keep_prob: .5})
+  temp = sess.run(modelResult, feed_dict={x_image: a, x_image_2: c, keep_prob: .5})
   f5 = open("/home/dhaslam/New_test_samples2/RotatedSet/labels/results3.txt","w+")  
   for i in range(0, 7167):
     if(temp[i][0] > temp[i][1] and temp[i][0] > temp[i][2]):
@@ -94,7 +100,7 @@ def getMRCDimensions(fileToOpen):
         zMax = zCoord
   return xMax, xMin, yMax, yMin, zMax, zMin
 
-def test(batch_Size):
+def test():
   keep_prob = tf.placeholder(tf.float32)
   xMax, xMin, yMax, yMin, zMax, zMin = getMRCDimensions(path2)
   xLength = xMax-xMin+1
@@ -102,8 +108,9 @@ def test(batch_Size):
   zLength = zMax-zMin+1
   numLines = (xLength)*(yLength)*(zLength)
   with open(path2) as inf2:
-    a = np.zeros((1,xLength, yLength, zLength, 3),dtype = float)
+    a = np.zeros((1,xLength, yLength, zLength, 1),dtype = float)
     b = np.zeros((1,numLines, 3), dtype = float)
+    c = np.zeros((1,xLength, yLength, zLength, 3), dtype = float)
     axisZ = np.zeros((1,numLines),dtype = float)
     axisY = np.zeros((1,numLines),dtype = float)
     axisX = np.zeros((1,numLines),dtype = float)
@@ -123,18 +130,55 @@ def test(batch_Size):
       thresh = float(thresh)
       label = int(label)
       curPos = (xCoord-xMin)*yLength*zLength+(yCoord-yMin)*zLength+(zCoord-zMin)
-      #print(curPos)
-      #print(str(xCoord-xMin) + " " + str(yCoord-yMin) + " " + str(zCoord-zMin))
       a[0][xCoord-xMin][yCoord-yMin][zCoord-zMin][0]  = thresh
-      a[0][xCoord-xMin][yCoord-yMin][zCoord-zMin][1]  = thresh
-      a[0][xCoord-xMin][yCoord-yMin][zCoord-zMin][2]  = thresh
       b[0][curPos][label] = 1
-  print(sess.run(accuracy, feed_dict={x_image: a, y_: b, keep_prob: 1.0}))
-  predictedLabels = sess.run(tf.argmax(modelResult,1), feed_dict={x_image: a})
-  writePredictionsToFile(predictedLabels, numLines, axisX, axisY, axisZ, b)
+  print(sess.run(accuracy, feed_dict={x_image: a, y_: b, x_image_2: c, keep_prob: 1.0}))
+  predictedLabels = sess.run(tf.argmax(modelResult,1), feed_dict={x_image: a, x_image_2: c})
+  writePredictionsToFile(predictedLabels, numLines, axisX, axisY, axisZ, b, 0)
+  
+def testSmallImages():
+  for i in range(0, 30):
+    path = path3 + str(i) + ".txt"
+    keep_prob = tf.placeholder(tf.float32)
+    xMax, xMin, yMax, yMin, zMax, zMin = getMRCDimensions(path)
+    xLength = xMax-xMin+1
+    yLength = yMax-yMin+1
+    zLength = zMax-zMin+1
+    numLines = (xLength)*(yLength)*(zLength)
+    with open(path) as inf2:
+      a = np.zeros((1,xLength, yLength, zLength, 1),dtype = float)
+      b = np.zeros((1,numLines, 3), dtype = float)
+      c = np.zeros((1,xLength, yLength, zLength, 3), dtype = float)
+      axisZ = np.zeros((1,numLines),dtype = float)
+      axisY = np.zeros((1,numLines),dtype = float)
+      axisX = np.zeros((1,numLines),dtype = float)
+      for x in range(0, xLength):
+        for y in range(0, yLength):
+          for z in range(0, zLength):
+            curPos = (x)*yLength*zLength+(y)*zLength+(z)
+            axisX[0][curPos] = x+xMin
+            axisY[0][curPos] = y+yMin
+            axisZ[0][curPos] = z+zMin
+      next(inf2)
+      for line3 in inf2:
+        xCoord, yCoord, zCoord, thresh, label = line3.strip().split(",")
+        xCoord = int(xCoord)
+        yCoord = int(yCoord)
+        zCoord = int(zCoord) 
+        thresh = float(thresh)
+        label = int(label)
+        curPos = (xCoord-xMin)*yLength*zLength+(yCoord-yMin)*zLength+(zCoord-zMin)
+        a[0][xCoord-xMin][yCoord-yMin][zCoord-zMin][0]  = thresh
+        b[0][curPos][label] = 1
+    acc = sess.run(accuracy, feed_dict={x_image: a, y_: b, x_image_2: c, keep_prob: 1.0})
+    print(acc)
+    fStat.write(str(acc) + '\n')
+    predictedLabels = sess.run(tf.argmax(modelResult,1), feed_dict={x_image: a, x_image_2: c})
+    writePredictionsToFile(predictedLabels, numLines, axisX, axisY, axisZ, b, i)
 
-def writePredictionsToFile(predictedLabels, numLines, axisX, axisY, axisZ, b):
-  f1 = open(tempPath,"w+")
+def writePredictionsToFile(predictedLabels, numLines, axisX, axisY, axisZ, b, index):
+  path = tempPath + str(index) + ".txt"
+  f1 = open(path,"w+")
   for i in range(0, numLines):
     f1.write(str(int(axisX[0][i])) + " " + str(int(axisY[0][i])) + " " + str(int(axisZ[0][i])) + " " + str(predictedLabels[i]) + "\r\n")
 
@@ -143,17 +187,19 @@ N = 32 #x dimension of training patch size
 M = 32 #y dimension of training patch size
 P = 7 #z dimension of training patch size
 batch_Size = 30
-epochs = 10000
+epochs = 10000000
 outputStats = "/home/dhaslam/New_test_samples2/RotatedSet/labels/Statistics.txt"
 trainingListLocation = "/home/dhaslam/New_test_samples2/RotatedSet/list.txt"
 path2 = "/home/dhaslam/New_test_samples2/RotatedSet/4XDA/4XDA_label.txt"
-tempPath = "/home/dhaslam/New_test_samples2/RotatedSet/labels/results.txt"
+path3 = "/home/dhaslam/New_test_samples2/RotatedSet/4XDA/4XDA-"
+tempPath = "/home/dhaslam/New_test_samples2/RotatedSet/labels/results"
 f5 = open("/home/dhaslam/New_test_samples2/RotatedSet/labels/results3.txt","w+")  
 
-x_image = tf.placeholder(tf.float32, shape=[None, None, None, None, 3])
+x_image = tf.placeholder(tf.float32, shape=[None, None, None, None, 1])
+x_image_2 = tf.placeholder(tf.float32, shape=[None, None, None, None, 3])
 y_ = tf.placeholder(tf.float32, shape=[None, None, 3])
 
-modelResult = model(x_image) #sets modelResults to the final result of the model 
+modelResult = model(x_image, x_image_2) #sets modelResults to the final result of the model 
 
 #train model
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=modelResult, labels=y_))
@@ -169,5 +215,6 @@ with tf.Session() as sess:
    for curEpoch in range(1, epochs):
      printEpoch(curEpoch) 
      train(trainingListLocation, batch_Size)
-     test(batch_Size)
+     #test()
+     testSmallImages()
    fStat.close()
