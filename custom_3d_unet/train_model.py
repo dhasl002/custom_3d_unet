7dd0b945a6aad9e5f11bf6bd7800102a93943de3
dhasl002 <dhasl002@odu.edu>
2019-09-19 23:52:37 -0700

    start of cleanup

---
+++ custom_3d_unet/train_model.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+from tensorflow.python import debug as tf_debug
+import numpy as np
+from custom_3d_unet import model
+from random import *
+from os import listdir
+from os.path import isfile, join
+from utils import *
+
+def train(batch_size, patch_width, patch_height, patch_depth):
+  data_files = [join("../data/", f) for f in listdir("../data/") if isfile(join("../data/", f))]
+  print(data_files)
+  for mrc_image_path in data_files:
+    a, b = read_mrc_image_data(mrc_image_path, patch_width, patch_height, patch_depth, batch_size)
+    if(curr_file_num % batch_Size == batch_Size - 1):
+      train_batch(a, b)
+
+def train_batch(a, b):
+  keep_prob = tf.placeholder(tf.float32)
+  current_loss = cross_entropy.eval(feed_dict={x_image: a, y_: b})
+  train_accuracy = accuracy.eval(feed_dict={x_image: a, y_: b, keep_prob: .5})
+  train_step.run(feed_dict={x_image: a, y_: b, keep_prob: .5})
+  print_batch_stats(train_accuracy, current_loss)
+
+if __name__ == "__main__":
+  print("hello world!")
+  patch_width = 48
+  patch_height = 48
+  patch_depth = 48
+  num_labels = 3
+  batch_Size = 1
+  epochs = 100000
+
+  x_image = tf.placeholder(tf.float32, shape=[None, patch_width, patch_height, patch_depth, 1])
+  y_ = tf.placeholder(tf.float32, shape=[None, None, num_labels])
+  modelResult = model(x_image)
+  cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=modelResult, labels=y_))
+  train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+  correct_prediction = tf.equal(tf.argmax(modelResult, 1), tf.argmax(tf.reshape(y_, [-1, num_labels]), 1))
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+  with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for curEpoch in range(1, epochs):
+      print('epoch Number: ' + str(curEpoch))
+      train(batch_Size, patch_width, patch_height, patch_depth)
+
---
+++ custom_3d_unet/utils.py
@@ -0,0 +1,78 @@
+import numpy as np
+from random import *
+
+def get_mrc_dimensions(file_to_open):
+  x_min = y_min = z_min= float("inf")
+  x_max = y_max = z_max = -1
+  with open(file_to_open) as input_file:
+    for line in input_file:
+      x_coord, y_coord, z_coord, thresh, label = line.strip().split(",")
+      x_coord = int(x_coord)
+      y_coord = int(y_coord)
+      z_coord = int(z_coord)
+      if x_coord < x_min:
+        x_min = x_coord
+      if y_coord < y_min:
+        y_min = y_coord
+      if z_coord < z_min:
+        z_min = z_coord
+      if x_coord > x_max:
+        x_max = x_coord
+      if y_coord > y_max:
+        y_max = y_coord
+      if z_coord > z_max:
+        z_max = z_coord
+
+  return x_max, x_min, y_max, y_min, z_max, z_min
+
+def writePredictionsToFile(predicted_labels, num_lines, index, axis_x, axis_y, axis_z):
+  output_path = "./" + str(index) + ".txt"
+  file = open(output_path,"w+")
+  for i in range(0, num_lines):
+    file.write(str(axis_x[0][i]) + " " + str(axis_y[0][i]) + " " + str(axis_z[0][i]) + " " + str(predicted_labels[i]) + "\r\n")
+
+def print_batch_stats(train_accuracy, current_loss):
+  print('training accuracy %g' % (train_accuracy))
+  print('Loss %g' % (current_loss))
+
+def select_patch_random(a, b, x_length, y_length, z_length, patch_width, patch_height, patch_depth):
+  x_start = randint(0, x_length - patch_width)
+  y_start = randint(0, y_length - patch_height)
+  z_start = randint(0, z_length - patch_depth)
+  a = np.delete(a, slice(0, x_start), axis=1)
+  a = np.delete(a, slice(patch_width, x_length - x_start), axis=1)
+  a = np.delete(a, slice(0, y_start), axis=2)
+  a = np.delete(a, slice(patch_height, y_length - y_start), axis=2)
+  a = np.delete(a, slice(0, z_start), axis=3)
+  a = np.delete(a, slice(patch_depth, z_length - z_start), axis=3)
+  b = np.delete(b, slice(0, x_start), axis=1)
+  b = np.delete(b, slice(patch_width, x_length - x_start), axis=1)
+  b = np.delete(b, slice(0, y_start), axis=2)
+  b = np.delete(b, slice(patch_height, y_length - y_start), axis=2)
+  b = np.delete(b, slice(0, z_start), axis=3)
+  b = np.delete(b, slice(patch_depth, z_length - z_start), axis=3)
+  b = np.reshape(b, (-1, patch_width * patch_height * patch_depth, 3))
+  return a, b
+
+def read_mrc_image_data(file_path, patch_width, patch_height, patch_depth, batch_size):
+  x_max, x_min, y_max, y_min, z_max, z_min = get_mrc_dimensions(file_path)
+  x_length = x_max - x_min + 1
+  y_length = y_max - y_min + 1
+  z_length = z_max - z_min + 1
+  with open(file_path) as f:
+    a = np.zeros((batch_size, x_length, y_length, z_length, 1), dtype = float)
+    b = np.zeros((batch_size, x_length, y_length, z_length, 3), dtype = float)
+    next(f)
+    for line in f:
+      x_coord, y_coord, z_coord, thresh, label = line.strip().split(",")
+      x_pos = int(x_coord) - x_min
+      y_pos = int(y_coord) - y_min
+      z_pos = int(z_coord) - z_min
+      thresh = float(thresh)
+      label = int(label)
+      a[0][x_pos][y_pos][z_pos][0] = thresh
+      b[0][x_pos][y_pos][z_pos][label] = 1
+      a, b = select_patch_random(a, b, x_length, y_length, z_length, patch_width, patch_height, patch_depth)
+  return a, b
+
+
